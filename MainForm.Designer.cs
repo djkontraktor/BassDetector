98692fd2459ce440e9e3134a7f655331e90adae5
WJC <kontractor.productions@gmail.com>
2024-11-30 18:46:55 -0800

    Still a runtime error

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ledPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ledPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ledPictureBox
+            // 
+            this.ledPictureBox.BackColor = System.Drawing.Color.Black;
+            this.ledPictureBox.Location = new System.Drawing.Point(73, 40);
+            this.ledPictureBox.Name = "ledPictureBox";
+            this.ledPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.ledPictureBox.TabIndex = 0;
+            this.ledPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ledPictureBox);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ledPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox ledPictureBox;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,14 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Dsp;
 
 namespace BassDetector
 {
     public partial class MainForm : Form
     {
+
+        private WasapiLoopbackCapture capture;
+        private BufferedWaveProvider buffer;
+        private Timer timer;
+        private bool ledOn = false;
+        private float A = 1;
+        private float fLow = 20;
+        private float fHigh = 100;
+
         public MainForm()
         {
             InitializeComponent();
+
+            // Set up audio capture
+            capture = new WasapiLoopbackCapture();
+            buffer = new BufferedWaveProvider(capture.WaveFormat);
+
+            capture.DataAvailable += (s, a) =>
+            {
+                buffer.AddSamples(a.Buffer, 0, a.BytesRecorded);
+            };
+
+            capture.StartRecording();
+
+            // Set up timer for analysis
+            timer = new Timer
+            {
+                Interval = 100 // Check every 100 milliseconds
+            };
+            timer.Tick += AnalyzeAudio;
+            timer.Start();
+        }
+
+        private void AnalyzeAudio(object sender, EventArgs e)
+        {
+            byte[] byteBuffer = new byte[buffer.BufferLength];
+            buffer.Read(byteBuffer, 0, buffer.BufferLength);
+
+            // Convert byte array to float array
+            float[] audioData = new float[byteBuffer.Length / 4];
+            for (int i = 0; i < audioData.Length; i++)
+            {
+                audioData[i] = BitConverter.ToSingle(byteBuffer, i * 4);
+            }
+
+            // Apply FFT
+            Complex[] fftBuffer = new Complex[audioData.Length];
+            for (int i = 0; i < audioData.Length; i++)
+            {
+                fftBuffer[i].X = (float)(audioData[i] * FastFourierTransform.HammingWindow(i, audioData.Length));
+                fftBuffer[i].Y = 0;
+            }
+
+            FastFourierTransform.FFT(true, (int)Math.Log(audioData.Length, 2.0), fftBuffer);
+
+            // Analyze specific frequency band
+            int sampleRate = capture.WaveFormat.SampleRate;
+            int lowIndex = (int)(fLow / sampleRate * audioData.Length);
+            int highIndex = (int)(fHigh / sampleRate * audioData.Length);
+
+            float amplitude = 0;
+            for (int i = lowIndex; i <= highIndex; i++)
+            {
+                amplitude += fftBuffer[i].X * fftBuffer[i].X + fftBuffer[i].Y * fftBuffer[i].Y;
+            }
+            amplitude = (float)Math.Sqrt(amplitude);
+
+            // Turn LED on or off based on amplitude
+            if (amplitude >= A)
+            {
+                ledOn = true;
+                ledPictureBox.BackColor = Color.Black;
+            }
+            else
+            {
+                ledOn = false;
+                ledPictureBox.BackColor = Color.Green;
+            }
         }
     }
 }
